253a453d89eab307ba77156476ec712e7f7e47a7
Julio Bergman <me@juliobergman.com>
2021-03-25 19:06:31 -0700

    Added Auth Scaffolding for vue

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,12 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import currentUser from "./modules/currentUser"
-
+import currentUser from "./modules/currentUser";
 
 Vue.use(Vuex);
 
-exporte default new Vuex.Store({
-    modules:{
+export default new Vuex.Store({
+    modules: {
         currentUser
     }
-})
+});

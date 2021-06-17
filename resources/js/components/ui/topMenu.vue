a8f02a27ef1257ec1c2f5da0fdc83e411160bd97
Julio Bergman <me@juliobergman.com>
2021-06-16 23:53:47 -0500

    fix top menu to

--- resources/js/components/ui/topMenu.vue
+++ resources/js/components/ui/topMenu.vue
@@ -58,7 +58,7 @@ export default {
             {
                 text: "My Account",
                 icon: "mdi-account",
-                to: "account"
+                to: "/account"
             },
             {
                 text: "New Company",

9e3fc589aa017f1a9d7cf18b265d2d5b60aaf212
Julio Bergman <me@juliobergman.com>
2021-06-28 14:47:14 -0500

    Financial Reports Files Created

---
+++ app/Http/Controllers/FinancialReportController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FinancialReport;
+use Illuminate\Http\Request;
+
+class FinancialReportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\FinancialReport  $financialReport
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FinancialReport $financialReport)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\FinancialReport  $financialReport
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FinancialReport $financialReport)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\FinancialReport  $financialReport
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FinancialReport $financialReport)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\FinancialReport  $financialReport
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FinancialReport $financialReport)
+    {
+        //
+    }
+}

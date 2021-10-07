67f2a0f5f52d10e33fcec7f6455791996527d44f
Julio Bergman <me@juliobergman.com>
2021-10-06 22:44:12 -0500

    force https on production

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -22,7 +22,7 @@ try {
 window.axios = require("axios");
 
 // window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-window.axios.defaults.baseURL = "http://unikk:4444/";
+// window.axios.defaults.baseURL = "http://unikk:4444/";
 window.axios.defaults.headers.common["Accept"] = "application/json";
 window.axios.defaults.headers.common["X-CSRF-TOKEN"] = $(
     'meta[name="csrf-token"]'
